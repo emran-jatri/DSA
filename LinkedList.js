e8a87c17a9d202ed76ab92beeb75d0bb3626edc7
Emran Ibn Shayed <89375788+emran-jatri@users.noreply.github.com>
2022-07-31 13:41:40 +0600

    Create LinkedList.js

---
+++ LinkedList.js
@@ -0,0 +1,53 @@
+
+
+class Node {
+  constructor(value){
+    this.value = value
+    this.next = null
+  }
+}
+
+class LinkedList{
+  constructor(){
+    this.head = null
+  }
+  addLast(value){
+    if(this.head === null){
+      this.head = new Node(value)
+    }
+    else{
+      // ---------------------- go to last node start ----------------------
+      let currentNode = this.head
+      while(currentNode.next !== null){
+        currentNode = currentNode.next
+      }
+      // ---------------------- go to last node end ----------------------
+      let newNode = new Node(value)
+      currentNode.next = newNode
+    }
+  }
+  printList(){
+    let printStr = ""
+    let currentNode = this.head
+    if(currentNode){
+      while(currentNode.next !== null){
+        printStr += `${currentNode.value} -> `
+        currentNode = currentNode.next
+      }
+      printStr += currentNode.value
+    }
+    else{
+      printStr = this
+    }
+    console.log(printStr)
+  }
+}
+
+
+let linkedList = new LinkedList();
+linkedList.addLast(1)
+linkedList.addLast(2)
+linkedList.addLast(3)
+
+linkedList.printList()
+
