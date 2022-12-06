dd3ae5e7562aa33a74e18c897c7dd78f50479cfd
emran-jatri <emran.jatri@gmail.com>
2022-12-06 16:46:32 +0600

    perfect binary tree added

---
+++ DataStructures/Tree/PerfectBinaryTree.js
@@ -0,0 +1,63 @@
+/* 
+
+- A perfect binary tree is a type of binary tree in which every internal node has exactly two child nodes and all the leaf nodes are at the same level.
+- depth{} start with 1
+- level{} start with 0
+
+
+*/
+
+class Node {
+  constructor(k) {
+    this.key = k;
+    this.right = this.left = null;
+  }
+}
+
+// # Calculate the depth
+const calculateDepth = (node) => {
+  d = 0;
+  while (node !== null) {
+    d += 1;
+    node = node.left;
+  }
+  return d;
+};
+
+// # Check if the tree is perfect binary tree
+const is_perfect = (root, d, level = 0) => {
+  // # Check if the tree is empty
+  if (root === null) {
+    return True;
+  }
+
+  // # Check the presence of trees
+  if (root.left === null && root.right === null) {
+    return d == level + 1;
+  }
+
+  if (root.left === null || root.right === null) {
+    return False;
+  }
+
+  return (
+    is_perfect(root.left, d, level + 1) && is_perfect(root.right, d, level + 1)
+  );
+};
+
+let root = null;
+root = new Node(1);
+root.left = new Node(2);
+root.right = new Node(3);
+
+root.left.left = new Node(4);
+root.left.right = new Node(5);
+
+root.right.left = new Node(6);
+root.right.right = new Node(7);
+
+if (is_perfect(root, calculateDepth(root))) {
+  console.log("The tree is a perfect binary tree");
+} else {
+  console.log("The tree is not a perfect binary tree");
+}
