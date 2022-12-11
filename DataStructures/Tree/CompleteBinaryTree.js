4531288d846bff3a963c4366d84c4d54f27fa53a
emran-jatri <emran.jatri@gmail.com>
2022-12-11 16:25:53 +0600

    priyority queue added

--- DataStructures/Tree/CompleteBinaryTree.js
+++ DataStructures/Tree/CompleteBinaryTree.js
@@ -17,35 +17,35 @@ class Node {
 }
 
 // Count the number of nodes
-const count_nodes = (root) => {
+const countNodes = (root) => {
   if (root === null) return 0;
-  return 1 + count_nodes(root.left) + count_nodes(root.right);
+  return 1 + countNodes(root.left) + countNodes(root.right);
 };
 
 // Check if the tree is complete binary tree
-const is_complete = (root, index, numberNodes) => {
+const isComplete = (root, index, numberNodes) => {
   // Check if the tree is empty
   if (root === null) return true;
 
   if (index >= numberNodes) return false;
 
   return (
-    is_complete(root.left, 2 * index + 1, numberNodes) &&
-    is_complete(root.right, 2 * index + 2, numberNodes)
+    isComplete(root.left, 2 * index + 1, numberNodes) &&
+    isComplete(root.right, 2 * index + 2, numberNodes)
   );
 };
 
-root = new Node(1);
+const root = new Node(1);
 root.left = new Node(2);
 root.right = new Node(3);
 root.left.left = new Node(4);
 root.left.right = new Node(5);
 root.right.left = new Node(6);
 
-node_count = count_nodes(root);
-index = 0;
+let nodeCount = countNodes(root);
+let index = 0;
 
-if (is_complete(root, index, node_count)) {
+if (isComplete(root, index, nodeCount)) {
   console.log("The tree is a complete binary tree");
 } else {
   console.log("The tree is not a complete binary tree");
