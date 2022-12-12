f208e49d769edffbc719ab2c69e52ce9d1e9e3c9
emran-jatri <emran.jatri@gmail.com>
2022-12-12 12:15:58 +0600

    heap updated

--- DataStructures/Heap/Heap.js
+++ DataStructures/Heap/Heap.js
@@ -1,6 +1,7 @@
-// Max-Heap data structure in Python
+// Max-Heap data structure in js
 
 function heapify(arr, arraySize, index) {
+
   largest = index;
   leftChildIndex = 2 * index + 1;
   rightChildIndex = 2 * index + 2;
@@ -14,51 +15,42 @@ function heapify(arr, arraySize, index) {
   }
 
   if (largest != index) {
-    // arr[index],arr[largest] = arr[largest],arr[index]
-    arr[index] = [arr[largest], arr[largest] = arr[index]][0]; // swap array
+    arr[index] = [arr[largest], (arr[largest] = arr[index])][0]; // swap array
     heapify(arr, arraySize, largest);
   }
 }
 
 function insert(array, newNum) {
-  let size = array.length;
-  if (size === 0) {
-    array.push(newNum);
-  } else {
-    array.push(newNum);
-    for (let i = Math.floor(size / 2) - 1; i > -1; i--) {
-      heapify(array, size, i);
-    }
-  }
-}
-
-function deleteNode(array, num) {
-  let size = array.length;
-  let i = 0;
-  for (i = 0; i < size; i++) {
-    if (num === array[i]) {
-      break;
-    }
-  }
-
-  // array[i], array[size - 1] = array[size - 1], array[i]
-  array[i] = [array[size - 1], (array[size - 1] = array[i])][0]; // swap array
-  array.pop(num);
-
-  for (i = Math.floor(array.length / 2) - 1; i > -1; i--) {
+  array.push(newNum);
+  for (let i = Math.floor(array.length / 2) - 1; i > -1; i--) { // only enter this loop if array length is 2
     heapify(array, array.length, i);
   }
 }
 
+function deleteNode(array, value) {
+	let indexOfValue = array.findIndex(item => value === item);
+
+	if (indexOfValue === -1) {
+		console.log(value, "not found in the array!")
+	}else {
+		array[indexOfValue] = [array[array.length - 1], (array[array.length - 1] = array[indexOfValue])][0]; // swap i and last element
+		array.pop();
+	
+		for (let i = Math.floor(array.length / 2) - 1; i > -1; i--) {
+			heapify(array, array.length, i);
+		}
+	}
+}
+
 const arr = [];
 
+insert(arr, 2);
+insert(arr, 15);
 insert(arr, 3);
 insert(arr, 4);
 insert(arr, 9);
-insert(arr, 5);
-insert(arr, 2);
 
 console.log("Max-Heap array: ", arr);
 
-deleteNode(arr, 4);
+deleteNode(arr, 9);
 console.log("After deleting an element: ", arr);
