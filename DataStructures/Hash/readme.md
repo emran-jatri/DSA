961d0e79a9b267e46790c481fdfee4a1957bec25
emran-jatri <emran.jatri@gmail.com>
2022-12-13 15:06:15 +0600

    hash added

---
+++ DataStructures/Hash/readme.md
@@ -0,0 +1,9 @@
+# Hash
+- The Hash table data structure stores elements in key-value pairs where
+- Key- unique integer that is used for indexing the values
+- Value - data that are associated with keys.
+
+# Hash Collision
+- When the hash function generates the same index for multiple keys, there will be a conflict (what value to be stored in that index). This is called a hash collision.
+
+- We can resolve the hash collision using one of the following techniques.
---
+++ DataStructures/Tree/BalanceBinaryTree.js
@@ -0,0 +1,9 @@
+/* 
+A balanced binary tree, also referred to as a height-balanced binary tree, is defined as a binary tree in which the height of the left and right subtree of any node differ by not more than 1.
+To learn more about the height of a tree/node, visit Tree Data Structure.Following are the conditions for a height-balanced binary tree:
+
+- difference between the left and the right subtree for any node is not more than one
+- the left subtree is balanced
+- the right subtree is balanced
+
+*/
